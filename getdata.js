f2564be1a4f361e3a462a583adda54fa6126fe69
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2015-04-03 18:14:47 +0200

    Change credentials

--- getdata.js
+++ getdata.js
@@ -4,8 +4,8 @@ module.exports=function(req,res){
 	var connection=mysql.createConnection({
 		host: process.env.OPENSHIFT_MYSQL_DB_HOST,
 		port: process.env.OPENSHIFT_MYSQL_DB_PORT,
-		user: process.env.OPENSHIFT_MYSQL_DB_USER,
-		password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+		user: "adminU56jbl7" || process.env.OPENSHIFT_MYSQL_DB_USER,
+		password: "bn82YgzLYJTv" || process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
 		database: "fliuva"
 	});
 	connection.query("SELECT * FROM EVENTS",function(err,results,fields){
--- server.js
+++ server.js
@@ -11,13 +11,7 @@ app.get("/collect",collect);
 app.get("/get",getdata);
 
 app.get("/all",function(req,res){
-	var connection=mysql.createConnection({
-		host: process.env.OPENSHIFT_MYSQL_DB_HOST,
-		port: process.env.OPENSHIFT_MYSQL_DB_PORT,
-		user: process.env.OPENSHIFT_MYSQL_DB_USER,
-		password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
-		database: "fliuva"
-	});
+	res.send(501,"Not implemented yet");
 });
 
 app.get("/uuid",function(req,res){
